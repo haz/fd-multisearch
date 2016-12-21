d03b43dc1217d63c58cd7d01f5b73d266f074cea
Christian Muise <christian.muise@gmail.com>
2016-12-21 16:20:26 -0500

    Getting a start on the multi-search test. Parsing of multiple goals / inits is complete, and the new search engine is copied from IteratedSearch.

---
+++ run.sh
@@ -0,0 +1,5 @@
+
+#./fast-downward.py --translate dom.pddl prob.pddl --translate-options --keep-unimportant-variables
+./build.py --debug -j 6
+./fast-downward.py --debug multi-output.sas --heuristic "h=ff(transform=adapt_costs(cost_type=ONE))" --search "multiple([lazy_greedy(h,preferred=[h])])"
+
