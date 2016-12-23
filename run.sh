51372e28593fb4ba8dc94123ba88bb183d48c76a
Christian Muise <christian.muise@gmail.com>
2016-12-23 14:14:43 -0500

    Simplifying the search engine, and fixing a goal reset bug.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 
 #./fast-downward.py --translate dom.pddl prob.pddl --translate-options --keep-unimportant-variables
 ./build.py --debug -j 6
-./fast-downward.py --debug multi-output.sas --search "multiple([lazy_greedy(ff())])"
+./fast-downward.py --debug multi-output.sas --search "multiple(lazy_greedy(ff()))"
 #./fast-downward.py --debug multi-output.sas --heuristic "h=ff(transform=adapt_costs(cost_type=ONE))" --search "multiple([lazy_greedy(h,preferred=[h])])"
+
+rm sas_plan.*
 
--- src/search/search_engines/multiple_search.cc
+++ src/search/search_engines/multiple_search.cc
@@ -10,7 +10,7 @@ using namespace std;
 namespace multiple_search {
 MultipleSearch::MultipleSearch(const Options &opts)
     : SearchEngine(opts),
-      engine_config(opts.get_list<ParseTree>("engine_configs")[0]),
+      engine_config(opts.get<ParseTree>("engine_config")),
       phase(0) {}
 
 SearchEngine *MultipleSearch::get_search_engine(int problem_index) {
@@ -23,6 +23,7 @@ SearchEngine *MultipleSearch::get_search_engine(int problem_index) {
     }
 
     cout << "\nNew Goal:" << endl;
+    g_goal.clear();
     for (auto varval : *(g_goal_MUISE[problem_index])) {
         cout << "var" << varval.first << " = " << varval.second << endl;
         g_goal.push_back(make_pair(varval.first, varval.second));
@@ -53,7 +54,6 @@ SearchStatus MultipleSearch::step() {
         return found_solution() ? SOLVED : FAILED;
     }
     
-    current_search->set_bound(999999);
     ++phase;
 
     current_search->search();
@@ -81,71 +81,16 @@ void MultipleSearch::print_statistics() const {
     statistics.print_detailed_statistics();
 }
 
-void MultipleSearch::save_plan_if_necessary() const {
-    // We don't need to save here, as we automatically save after
-    // each successful search iteration.
-}
+void MultipleSearch::save_plan_if_necessary() const { }
 
 static SearchEngine *_parse(OptionParser &parser) {
-    parser.document_synopsis("Multiple search", "");
-    parser.document_note(
-        "Note 1",
-        "We don't cache heuristic values between search iterations at"
-        " the moment. If you perform a LAMA-style iterative search,"
-        " heuristic values will be computed multiple times.");
-    parser.document_note(
-        "Note 2",
-        "The configuration\n```\n"
-        "--search \"multiple([lazy_wastar(merge_and_shrink(),w=10), "
-        "lazy_wastar(merge_and_shrink(),w=5), lazy_wastar(merge_and_shrink(),w=3), "
-        "lazy_wastar(merge_and_shrink(),w=2), lazy_wastar(merge_and_shrink(),w=1)])\"\n"
-        "```\nwould perform the preprocessing phase of the merge and shrink heuristic "
-        "5 times (once before each iteration).\n\n"
-        "To avoid this, use heuristic predefinition, which avoids duplicate "
-        "preprocessing, as follows:\n```\n"
-        "--heuristic \"h=merge_and_shrink()\" --search "
-        "\"multiple([lazy_wastar(h,w=10), lazy_wastar(h,w=5), lazy_wastar(h,w=3), "
-        "lazy_wastar(h,w=2), lazy_wastar(h,w=1)])\"\n"
-        "```");
-    parser.document_note(
-        "Note 3",
-        "If you reuse the same landmark count heuristic "
-        "(using heuristic predefinition) between iterations, "
-        "the path data (that is, landmark status for each visited state) "
-        "will be saved between iterations.");
-    parser.add_list_option<ParseTree>("engine_configs",
-                                      "list of search engines for each phase");
-    parser.add_option<bool>(
-        "pass_bound",
-        "use bound from previous search. The bound is the real cost "
-        "of the plan found before, regardless of the cost_type parameter.",
-        "true");
-    parser.add_option<bool>("repeat_last",
-                            "repeat last phase of search",
-                            "false");
-    parser.add_option<bool>("continue_on_fail",
-                            "continue search after no solution found",
-                            "false");
-    parser.add_option<bool>("continue_on_solve",
-                            "continue search after solution found",
-                            "true");
+    parser.add_option<ParseTree>("engine_config", "search engine for use in all phases");
     SearchEngine::add_options_to_parser(parser);
     Options opts = parser.parse();
-
-    opts.verify_list_non_empty<ParseTree>("engine_configs");
-
-    if (parser.help_mode()) {
+    if (parser.help_mode())
         return nullptr;
-    } else if (parser.dry_run()) {
-        //check if the supplied search engines can be parsed
-        for (const ParseTree &config : opts.get_list<ParseTree>("engine_configs")) {
-            OptionParser test_parser(config, true);
-            test_parser.start_parsing<SearchEngine *>();
-        }
-        return nullptr;
-    } else {
+    else
         return new MultipleSearch(opts);
-    }
 }
 
 static Plugin<SearchEngine> _plugin("multiple", _parse);
