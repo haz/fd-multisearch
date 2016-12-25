03c883c87a591b862c8b82ca4c1c225bdded5d32
Christian Muise <christian.muise@gmail.com>
2016-12-25 15:06:54 -0500

    Updating the build parameter properly.

--- run.sh
+++ run.sh
@@ -3,16 +3,19 @@
 #./fast-downward.py --debug multi-output.sas --search "multiple(lazy_greedy(ff()))"
 #./fast-downward.py --debug multi-output.sas --heuristic "h=ff(transform=adapt_costs(cost_type=ONE))" --search "multiple(lazy_greedy(h,preferred=[h]))"
 
+#BUILD=debug32
+BUILD=release64
+
 if [[ $# -eq 0 ]] ; then
-    ./fast-downward.py multi-output.sas --search "multiple(lazy_greedy(ff()))"
+    ./fast-downward.py --build="$BUILD" multi-output.sas --search "multiple(lazy_greedy(ff()))"
 elif [[ "$1" == "build" ]] ; then
-    ./build.py -j 6 release64
+    ./build.py -j 6 "$BUILD"
 elif [[ "$1" == "memory" ]] ; then
-    valgrind --leak-check=yes ./builds/release64/bin/downward --search "multiple(lazy_greedy(ff()))" --internal-plan-file sas_plan < multi-output.sas > /dev/null 2>output.log
+    valgrind --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple(lazy_greedy(ff()))" --internal-plan-file sas_plan < multi-output.sas > /dev/null 2>output.log
     gedit output.log
     rm output.log
 elif [[ "$1" == "profile" ]] ; then
-    valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/debug32/bin/downward --search "multiple(lazy_greedy(ff()))" --internal-plan-file sas_plan < multi-output.sas > /dev/null
+    valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/$BUILD/bin/downward --search "multiple(lazy_greedy(ff()))" --internal-plan-file sas_plan < multi-output.sas > /dev/null
     #kcachegrind callgrind.out.*
     python gprof2dot.py --format=callgrind --output=out.dot callgrind.out.*
     dot -T png out.dot -o out.png
