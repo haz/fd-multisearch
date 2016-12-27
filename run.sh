e0a7451d25e00db16f3b3fc56c7b9397d416b313
Christian Muise <christian.muise@gmail.com>
2016-12-27 12:54:25 -0500

    Put in better memory profiling, and simplified the initial state and goal resetting.

--- run.sh
+++ run.sh
@@ -6,17 +6,21 @@
 #BUILD=debug32
 BUILD=release64
 
-#TARGET=multi-output-small.sas
-TARGET=multi-output-big.sas
+TARGET=multi-output-small.sas
+#TARGET=multi-output-big.sas
 
 if [[ $# -eq 0 ]] ; then
     ./fast-downward.py --build="$BUILD" "$TARGET" --search "multiple()"
 elif [[ "$1" == "build" ]] ; then
     ./build.py -j 6 "$BUILD"
-elif [[ "$1" == "memory" ]] ; then
-    valgrind --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < "$TARGET" > /dev/null 2>output.log
+elif [[ "$1" == "memleak" ]] ; then
+    valgrind --tool=memcheck --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < "$TARGET" > output.log 2>&1
     gedit output.log
     rm output.log
+elif [[ "$1" == "memuse" ]] ; then
+    valgrind --tool=massif --stacks=yes --massif-out-file=massif.log ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < "$TARGET" > /dev/null 2>&1
+    ms_print massif.log
+    rm -rf massif.log
 elif [[ "$1" == "profile" ]] ; then
     valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < "$TARGET" > /dev/null
     kcachegrind callgrind.out.*
