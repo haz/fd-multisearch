f5e24dfd802b075f857a7b6c15f876c40074b65f
Christian Muise <christian.muise@gmail.com>
2016-12-27 13:29:55 -0500

    Only creating the heuristic once.

--- src/search/search_engines/multiple_search.h
+++ src/search/search_engines/multiple_search.h
@@ -4,6 +4,7 @@
 #include "../option_parser_util.h"
 #include "../search_engine.h"
 #include "../utils/timer.h"
+#include "../open_lists/standard_scalar_open_list.h"
 
 #include <memory>
 
@@ -13,12 +14,16 @@ class Options;
 
 namespace multiple_search {
 class MultipleSearch : public SearchEngine {
+
     int phase;
+    ScalarEvaluator *h;
 
     std::unique_ptr<SearchEngine> get_search_engine(int problem_index);
     std::unique_ptr<SearchEngine> create_phase(int phase);
 
     virtual SearchStatus step() override;
+
+    std::unique_ptr<SearchEngine> build_lazy_ff_search();
 
 public:
     explicit MultipleSearch(const options::Options &opts);
