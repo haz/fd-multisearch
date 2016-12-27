16929a75b0d3878019dbb0f3fd3d122b41511821
Christian Muise <christian.muise@gmail.com>
2016-12-27 11:07:02 -0500

    Updating the run script to the new syntax.

--- run.sh
+++ run.sh
@@ -7,18 +7,17 @@
 BUILD=release64
 
 if [[ $# -eq 0 ]] ; then
-    ./fast-downward.py --build="$BUILD" multi-output.sas --search "multiple(lazy_greedy(ff()))"
+    ./fast-downward.py --build="$BUILD" multi-output.sas --search "multiple()"
 elif [[ "$1" == "build" ]] ; then
     ./build.py -j 6 "$BUILD"
 elif [[ "$1" == "memory" ]] ; then
-    valgrind --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple(lazy_greedy(ff()))" --internal-plan-file sas_plan < multi-output.sas > /dev/null 2>output.log
+    valgrind --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < multi-output.sas > /dev/null 2>output.log
     gedit output.log
     rm output.log
 elif [[ "$1" == "profile" ]] ; then
-    valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/$BUILD/bin/downward --search "multiple(lazy_greedy(ff()))" --internal-plan-file sas_plan < multi-output.sas > /dev/null
+    valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < multi-output.sas > /dev/null
     #kcachegrind callgrind.out.*
-    python gprof2dot.py --format=callgrind --output=out.dot callgrind.out.*
-    dot -T png out.dot -o out.png
+    python gprof2dot.py --format=callgrind --output=out.dot callgrind.out.*; dot -T png out.dot -o out.png
     rm -rf callgrind.out.*
     rm -rf out.dot
 fi
