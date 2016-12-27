d46639c86de7ab8835058e4f025a5d5e95b4b292
Christian Muise <christian.muise@gmail.com>
2016-12-27 11:39:27 -0500

    Renaming the output file based on size.

--- run.sh
+++ run.sh
@@ -6,18 +6,21 @@
 #BUILD=debug32
 BUILD=release64
 
+#TARGET=multi-output-small.sas
+TARGET=multi-output-big.sas
+
 if [[ $# -eq 0 ]] ; then
-    ./fast-downward.py --build="$BUILD" multi-output.sas --search "multiple()"
+    ./fast-downward.py --build="$BUILD" "$TARGET" --search "multiple()"
 elif [[ "$1" == "build" ]] ; then
     ./build.py -j 6 "$BUILD"
 elif [[ "$1" == "memory" ]] ; then
-    valgrind --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < multi-output.sas > /dev/null 2>output.log
+    valgrind --leak-check=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < "$TARGET" > /dev/null 2>output.log
     gedit output.log
     rm output.log
 elif [[ "$1" == "profile" ]] ; then
-    valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < multi-output.sas > /dev/null
-    #kcachegrind callgrind.out.*
-    python gprof2dot.py --format=callgrind --output=out.dot callgrind.out.*; dot -T png out.dot -o out.png
+    valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes ./builds/$BUILD/bin/downward --search "multiple()" --internal-plan-file sas_plan < "$TARGET" > /dev/null
+    kcachegrind callgrind.out.*
+    #python gprof2dot.py --format=callgrind --output=out.dot callgrind.out.*; dot -T png out.dot -o out.png
     rm -rf callgrind.out.*
     rm -rf out.dot
 fi
